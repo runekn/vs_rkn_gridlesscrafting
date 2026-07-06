40dc7e996ee9a99e3ae3a031176913262a7a3562
Rune Kofoed-Nissen <rune@beast.dk>
2026-07-06 08:54:05 +0200

    Better fix

--- GridlessCrafting/BlockBehaviorSpawnCraftingSurface.cs
+++ GridlessCrafting/BlockBehaviorSpawnCraftingSurface.cs
@@ -1,6 +1,8 @@
+using System.Numerics;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
+using Vintagestory.Common;
 
 namespace RKN.GridlessCrafting;
 
@@ -21,13 +23,14 @@ public class BlockBehaviorSpawnCraftingSurface : BlockBehavior
         {
             return true;
         }
-        // TODO: Causes crash when spawning with block as initial ingredient. Because default action is not being prevented, and it when fails with NPE because block is already removed.
-        /*bool r = (world.GetBlock(new AssetLocation("rkngridlesscrafting:craftingsurface")) as BlockCrafting).TryPlace(byPlayer, blockSel.Position, byPlayer.InventoryManager.ActiveHotbarSlot);
+        bool r = (world.GetBlock(new AssetLocation("rkngridlesscrafting:craftingsurface")) as BlockCrafting).TryPlace(byPlayer, blockSel.Position, byPlayer.InventoryManager.ActiveHotbarSlot);
         if (!r) {
             return true;
-        }*/
+        }
         clientApi.Network.GetChannel("rkngridlesscrafting").SendPacket(new CreateCraftingBlockMessage() { Position = blockSel.Position });
         handling = EnumHandling.PreventSubsequent;
-        return false; // Prevent default server message as we have done that ourselves
+        // It would be better if I could return false to prevent default server message as we have done that ourselves.
+        // But that will also enable default behavior to place block, which causes game to crash because we have now removed the block from inventory.
+        return true; 
     }
 }
